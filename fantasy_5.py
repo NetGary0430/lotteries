57e85b4e0865c92f160550068837f3f4a6f47991
Gary Netherton <11843494+NetGary0430@users.noreply.github.com>
2025-05-26 13:32:09 -0700

    refactor: Added all 5 and 6 digit pick games.
    This adds all pick 5 and pick 6 lotteries.
    
    BREAKING CHANGE
    None

---
+++ fantasy_5.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from pypdf import PdfReader
+import tabula
+from downloadANDread import download_and_read_pdf
+
+# Read PDF into list of DataFrame
+
+
+pdf_url = "https://files.arizonalottery.com/past-180-days/Past180Days_Fantasy_5.pdf" # Example URL
+output_file = "fantasy5.pdf"
+download_and_read_pdf(pdf_url, output_file)
+
+inputPDF = "fantasy5.pdf"
+
+pdf_path = inputPDF
+pdf_file = open(pdf_path, 'rb')
+pdf_reader = PdfReader(pdf_file)
+
+tables = []
+for page_num in range(len(pdf_reader.pages)):
+    table = tabula.read_pdf(pdf_path, pages=page_num + 1, stream=True)[0]
+    tables.append(table)
+
+df = pd.concat(tables, ignore_index=True)
+
+print(df)
+outputPDF = "D:\\MyCode\\python\\W2calculator\\lotto_results\\Fantasy_Five.xlsx"
+df.to_excel(outputPDF)
